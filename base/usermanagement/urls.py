a693e95766c1f03205f771b5fdc3f77dac631850
sudipraaz <happysudip440@gmail.com>
2024-08-28 11:22:48 +0545

    adding doctor configuration

--- base/usermanagement/urls.py
+++ base/usermanagement/urls.py
@@ -11,8 +11,6 @@ urlpatterns = [
     path('register/', views.UserCreate.as_view(), name='user-register'),
     path('create_superuser/', views.create_superuser, name='create-superuser'),
     # path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-
-   
     path('login/', views.LoginView.as_view(), name='token_obtain_pair'),     # login with cookies set 
     path('login/token-refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('users/', views.UserProfile.as_view(), name='user list'),  # get the list of all users
